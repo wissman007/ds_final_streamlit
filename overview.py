824999aa6e9cb1ab542329fba4341e5a5fb00673
Christophe Pimm <toph.pimm@gmail.com>
2025-04-09 15:10:05 +0200

    Update data and add intro

---
+++ overview.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Overview")
+
+st.header("Starting point - the xView2 challenge")
+
+st.markdown("""
+* **GOAL**:
+    * Asses building damage
+    * Localize Buildings and Score Damage     
+""")
