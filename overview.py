69f29b12b699977c266caf2f2113642e1f8f0060
Christophe Pimm <toph.pimm@gmail.com>
2025-04-09 15:43:07 +0200

    Continue intro

--- overview.py
+++ overview.py
@@ -6,6 +6,23 @@ st.header("Starting point - the xView2 challenge")
 
 st.markdown("""
 * **GOAL**:
-    * Asses building damage
-    * Localize Buildings and Score Damage     
+    * Automatically assess building damage.
+    * Localize Buildings and Score Damage.
+* **AIM**: Help disaster response teams to assess damage rapidly and allocate resources effectively when a disaster occur
 """)
+
+st.header("The dataset")
+
+col1, col2, col3 = st.columns(3)
+
+col1.success("18 336 images")
+col2.success("850 000 buildings")
+col3.success("15 countries")
+
+col1.image("data/pre-intro.jpg")
+col2.image("data/post-intro.jpg")
+col3.image("data/mask-intro.jpg")
+
+st.header("Approach")
+
+st.write("Image segmentation")
